73f06a3d5bf9c75dafdae9e925ebcb695e3e223e
John Okyere-Gyamfi <johnog@OGMBP13.local>
2018-12-23 11:08:30 +0000

    update product listing page UI

---
+++ src/main/java/com/integra/store/domain/enumeration/InvoiceStatus.java
@@ -0,0 +1,8 @@
+package com.integra.store.domain.enumeration;
+
+/**
+ * The InvoiceStatus enumeration.
+ */
+public enum InvoiceStatus {
+    PAID, ISSUED, CANCELLED
+}
---
+++ src/main/java/com/integra/store/domain/enumeration/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.integra.store.domain.enumeration;
+
+/**
+ * The OrderStatus enumeration.
+ */
+public enum OrderStatus {
+    COMPLETED, PENDING, CANCELLED
+}
